12c89d852eb2ce24904b13021224eaf7a31ec12b
Rodrigo Saboya <saboya@users.noreply.github.com>
2017-12-05 09:59:03 -0200

    Adding plugins entry point tests.

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import ModuleDefaultComponent, { Dygraph, FixedYAxis } from '../src'
+import ModuleDefaultComponent, { Dygraph, FixedYAxis, Normalize, Downsample, StickyEdges } from '../src'
 
 describe('module entry point', () => {
   it('should be a function', () => {
@@ -11,8 +11,17 @@ describe('module entry point', () => {
   })
 })
 
-describe('FixedYAxis entry point', () => {
-  it('should be a function', () => {
+describe('Plugins entry points', () => {
+  it('FixedYAxis should be a function', () => {
     expect(FixedYAxis).to.be.a('function')
+  })
+  it('Normalize should be a function', () => {
+    expect(Normalize).to.be.a('function')
+  })
+  it('Downsample should be a function', () => {
+    expect(Downsample).to.be.a('function')
+  })
+  it('StickyEdges should be a function', () => {
+    expect(StickyEdges).to.be.a('function')
   })
 })
