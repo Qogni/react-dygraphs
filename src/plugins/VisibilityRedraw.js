f4200877b40426dd8e0785db01b283c72b9ae0fa
Rodrigo Saboya <saboya@users.noreply.github.com>
2018-02-21 22:59:21 -0300

    Fixing VisibilityRedraw plugin.

--- src/plugins/VisibilityRedraw.js
+++ src/plugins/VisibilityRedraw.js
@@ -1,28 +1,41 @@
+import { addEvent, removeEvent } from 'dygraphs/src/dygraph-utils'
+
+const crossVisibilityChange = () => {
+  if (typeof document.hidden !== 'undefined') {
+    return 'visibilitychange'
+  } else if (typeof document.msHidden !== 'undefined') {
+    return 'msvisibilitychange'
+  } else if (typeof document.webkitHidden !== 'undefined') {
+    return 'webkitvisibilitychange'
+  }
+}
+
+const crossHidden = () => {
+  if (typeof document.hidden !== 'undefined') {
+    return 'hidden'
+  } else if (typeof document.msHidden !== 'undefined') {
+    return 'msHidden'
+  } else if (typeof document.webkitHidden !== 'undefined') {
+    return 'webkitHidden'
+  }
+}
+
 export default class VisibilityRedraw {
   static toString = () => {
     return 'VisibilityRedraw Plugin'
   }
 
-  activate (dygraph) {
-    let hidden, visibilityChange
-
-    if (typeof document.hidden !== 'undefined') {
-      hidden = 'hidden'
-      visibilityChange = 'visibilitychange'
-    } else if (typeof document.msHidden !== 'undefined') {
-      hidden = 'msHidden'
-      visibilityChange = 'msvisibilitychange'
-    } else if (typeof document.webkitHidden !== 'undefined') {
-      hidden = 'webkitHidden'
-      visibilityChange = 'webkitvisibilitychange'
-    }
-
-    const handleVisibilityChange = (e) => {
-      if (!document[hidden]) {
+  activate = (dygraph) => {
+    this.handleVisibilityChange = (e) => {
+      if (!document[crossHidden()]) {
         dygraph.updateOptions({}, false)
       }
     }
 
-    document.addEventListener(visibilityChange, handleVisibilityChange, false)
+    addEvent(document, crossVisibilityChange(), this.handleVisibilityChange)
+  }
+
+  destroy = (e) => {
+    removeEvent(document, crossVisibilityChange(), this.handleVisibilityChange)
   }
 }
