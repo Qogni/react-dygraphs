5c84e81adadac96888283df691b57bcf7038a22e
Rodrigo Saboya <saboya@users.noreply.github.com>
2018-02-26 18:09:08 -0300

    Handling cases where the valueRange is updated with different values.

--- src/plugins/FixedYAxis.js
+++ src/plugins/FixedYAxis.js
@@ -4,14 +4,23 @@ export default class FixedYAxis {
   }
 
   activate (dygraph) {
-    const valueRange = dygraph.getOption('valueRange')
+    let valueRange = dygraph.getOption('valueRange')
 
     if (!valueRange) {
       throw new Error('No valueRange set')
     }
 
     const getExtremeYValues = () => valueRange
-    const predraw = (e) => (e.dygraph.dataHandler_.getExtremeYValues = getExtremeYValues)
+    const predraw = (e) => {
+      let newRange = dygraph.getOption('valueRange')
+
+      if (valueRange[0] !== newRange[0] ||
+      valueRange[1] !== newRange[1]) {
+        valueRange = newRange
+      }
+
+      e.dygraph.dataHandler_.getExtremeYValues = getExtremeYValues
+    }
 
     // Default value of 0.1 will add 10% Y-axis padding
     dygraph.updateOptions({ yRangePad: 0 }, true)
